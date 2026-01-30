70419b02c1fab66ba5bbdcbe6fb4a53d40a32efa
IMNotMax <imreallynotmax@gmail.com>
2026-01-29 22:18:19 -0500

    Modification de la nomenclature des ficheir .env et ajout de la fonction de suppression des domaines orphelins

--- install.sh
+++ install.sh
@@ -2,11 +2,12 @@
 
 cp npm-sync /usr/local/bin/
 #vérifier que /usr/local/bin est dans le PATH, sinon l'ajouter
+TARGET_ENV_FILE=/root/.env_npm-sync
 
 chmod +x /usr/local/bin/npm-sync
-cp .env.example /root/.env #ne pas écraser si un fichier existe
-chmod 600 /root/.env
-chown root:root /root/.env
+cp .env_npm-sync_example $TARGET_ENV_FILE #ne pas écraser si un fichier existe
+chmod 600 $TARGET_ENV_FILE
+chown root:root $TARGET_ENV_FILE
 
 #écrire un message en anglais et en francais pour informer que crontab -e peut être modifié avec */15 * * * * /usr/local/bin/npm-sync >> /var/log/npm-sync.log 2>&1 et expliquer cette ligne OU que la synchro peut être simplement lancée avec npm-sync
 
